913be870c30c518722f4957ce769c5525fbaa731
Nursultan-Salikhov <84440114+Nursultan-Salikhov@users.noreply.github.com>
2023-05-07 12:23:24 +0300

    Added repository

---
+++ internal/repository/repository.go
@@ -0,0 +1,20 @@
+package repository
+
+import "Todo_list/internal/model"
+
+type Task interface {
+	Create(task model.Task) (int, error)
+	GetAll() ([]*model.Task, error)
+	GetCompleted() ([]*model.Task, error)
+	GetUncompleted() ([]*model.Task, error)
+	Update(taskId int, updateTask model.UpdateTask) error
+	Delete(taskId int) error
+}
+
+type Repository struct {
+	Task
+}
+
+func NewRepository(task Task) *Repository {
+	return &Repository{Task: task}
+}
